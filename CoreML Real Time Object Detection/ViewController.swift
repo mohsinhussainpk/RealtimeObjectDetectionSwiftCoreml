20fbf69f47a1b02a4f134cf5893b402025509b3a
axiom3 <axiom3@axiom3s-iMac.local>
2018-07-05 21:42:22 +0500

    realtime object detection on ios with coreml

--- CoreML Real Time Object Detection/ViewController.swift
+++ CoreML Real Time Object Detection/ViewController.swift
@@ -7,17 +7,64 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice =
+            AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device:
+            captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session:
+            captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+        
+       
+//        VNImageRequestHandler(cgImage: <#T##CGImage#>, options: [:]).perform(<#T##requests: [VNRequest]##[VNRequest]#>)
+        
+            }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        //print("Camera was able to capture a frame", Date())
+        
+        guard let pixelBuffer: CVPixelBuffer =
+        CMSampleBufferGetImageBuffer(sampleBuffer) else
+        { return }
+      
+        guard let model = try? VNCoreMLModel(for: SqueezeNet().model) else
+        { return }
+        let request = VNCoreMLRequest(model: model)
+        { (finishedReq, err) in
+            
+            print(finishedReq.results)
+            
+            guard let results = finishedReq.results as?
+                [VNClassificationObservation] else { return }
+            
+            guard let firstObservation = results.first else
+            { return }
+            
+            print(firstObservation.identifier,
+                  firstObservation.confidence)
+        }
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
     }
 
 
